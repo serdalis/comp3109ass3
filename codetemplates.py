fd1a100afe9e4349b1b51bd9d42351f75b1f40ee
jpearse <josh.pearse@gmail.com>
2010-10-22 09:25:41 +1100

    referencing fixes in templates

--- codetemplates.py
+++ codetemplates.py
@@ -35,14 +35,16 @@ localaddr_template = '''
 	imull $4s, %%eax, %%eax
 	addl $16, %%eax
 	imull $%(var_num)s, %%eax, %%eax
-	subl %ebp, %(destreg)s
-	negl %(destreg)s
-	andl $-16, %(destreg)s
+	subl %%ebp, %%eax
+	negl %%eax
+	andl $-16, %%eax
 '''
 # place constant val into destreg
-const_template = '''
-	movl $.const%(val)s, %(destreg)s
+constaddr_template = '''
+	movl $.const%(val)s, %%eax
+'''
 
+consttable_template = '''
 	.data
 	.align 16
 	.const%(val)s:
@@ -56,16 +58,16 @@ equ_template = '''
 	<load source address into %eax>
 	<load destination address into %edx>
 
-	movl 8(%ebp), %ecx
-	shrl $2, %ecx
+	movl 8(%%ebp), %%ecx
+	shrl $2, %%ecx
 	jz .loop_end<X>
 
 	.loop_begin<X>:
-		movaps (%eax), %xmm0
-		movaps %xmm0, (%edx)
+		movaps (%%eax), %%xmm0
+		movaps %%xmm0, (%%edx)
 
-		addl $16, %eax #add this line if %eax is not pointing to a constant
-		addl $16, %edx
+		addl $16, %%eax #add this line if %eax is not pointing to a constant
+		addl $16, %%edx
 		loopl .loop_begin<X>
 	.loop_end<X>:
 '''
@@ -75,33 +77,33 @@ equWithOp_template = '''
 	<load source2 address into %ebx>
 	<load destination address into %edx>
 
-	movl 8(%ebp), %ecx
-	shrl $2, %ecx
+	movl 8(%%ebp), %%ecx
+	shrl $2, %%ecx
 	jz .loop_end<X>
 
 	.loop_begin<X>:
-		movaps (%eax), %xmm0
-		movaps (%ebx), %xmm1
-		<operation> %xmm0, %xmm1
-		movaps %xmm1, (%edx)
+		movaps (%%eax), %%xmm0
+		movaps (%%ebx), %%xmm1
+		<operation> %%xmm0, %%xmm1
+		movaps %%xmm1, (%%edx)
 
-		addl $16, %eax #add this line if %eax is not pointing to a constant
-		addl $16, %ebx #add this line if %ebx is not pointing to a constant
-		addl $16, %edx
+		addl $16, %%eax #add this line if %eax is not pointing to a constant
+		addl $16, %%ebx #add this line if %ebx is not pointing to a constant
+		addl $16, %%edx
 		loopl .loop_begin<X>
 	.loop_end<X>:
 '''
 #invoke a function
 invoke_template = '''
 	# invoke function <name>
-	subl $4*<N+1>, %esp # grow the stack
-	movl <argN>, %eax # setup argN
-	movl %eax, <4*N>(%esp)
+	subl $4*<N+1>, %%esp # grow the stack
+	movl <argN>, %%eax # setup argN
+	movl %eax, <4*N>(%%esp)
 ...
-	movl <arg1>, %eax # setup arg1
-	movl %eax, 4(%esp)
-	movl 8(%ebp), %eax # setup implicit vector length
-	movl %eax, 0(%esp)
+	movl <arg1>, %%eax # setup arg1
+	movl %%eax, 4(%%esp)
+	movl 8(%%ebp), %%eax # setup implicit vector length
+	movl %%eax, 0(%%esp)
 	call <name> # invoke the function
-	addl $4*<N+1>, %esp # restore the stack
+	addl $4*<N+1>, %%esp # restore the stack
 '''
