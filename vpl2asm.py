eb84a918656df789bdec19a7833bc5b7524f2f6b
Pearse; Joshua David <Pearse; Joshua David jpea5904@ug.it.usyd.edu.au>
2010-10-22 11:22:38 +1100

    renamed + updated prog.py

--- vpl2asm.py
+++ vpl2asm.py
@@ -6,6 +6,7 @@ import antlr3.tree
 from VPLLexer import VPLLexer
 from VPLParser import VPLParser
 import util
+from codetemplates import *
 
 char_stream = antlr3.ANTLRInputStream(sys.stdin)
 lexer = VPLLexer(char_stream)
@@ -16,6 +17,8 @@ r = parser.start()
 root = r.tree
 
 sys.stderr.write(util.print_tree(root))
+
+print func_template % {"name": str(root.children[0]), "body": ""}
 
 #nodes = antlr3.tree.CommonTreeNodeStream(root)
 #nodes.setTokenStream(tokens)
