e25443748f3453f24db30fa40bf22a900b1079bf
Pearse; Joshua David <Pearse; Joshua David jpea5904@ug.it.usyd.edu.au>
2010-10-22 18:00:38 +1100

    as above

--- main.c
+++ main.c
@@ -1,29 +1,39 @@
 #include <stdlib.h> 
+#include <math.h>
+#include <time.h>
 
 /* alignment macro: aligns a memory block a to multiplies of a */ 
-#define align(s,a) (((size_t)(s) + ((a) - 1)) & ~((size_t)(a) - 1))
+#define align(s,a) (((size_t)(s) + ((a) - 1)) & ~((size_t) (a) - 1))
 /* Alignment for SSE unit */ 
 #define SSE_ALIGN (16)
 /* Number of elements */ 
-#define NUM (100) 
+#define NUM (10)
 
 extern void foo(int, float *, float *);
 int main(void) 
 { 	
+	int i;
 	float *a = malloc(sizeof(float)*NUM + SSE_ALIGN),
-		  *b = malloc(sizeof(float)*NUM + SSE_ALIGN), 
+		  *b = malloc(sizeof(float)*NUM + SSE_ALIGN),  
 		  *c = malloc(sizeof(float)*NUM + SSE_ALIGN); 	
 	/* make sure that pointers are aligned to multiplies of 16 bytes */
 	a= 	(float *) align(a, SSE_ALIGN);
 	b= 	(float *) align(b, SSE_ALIGN);
 	c= 	(float *) align(c, SSE_ALIGN);
 	
-	//foo(NUM,a);
 	foo(NUM, a, b);
-	int i;
-	for (i = 0; i != SSE_ALIGN; i++) {
-		printf("%f\n%f\n", a[i], b[i]);
-	}
+
+	for (i = 0; i < NUM; i++)
+		printf("Vector: a[%d] -> %f\n", i, a[i]);
+	printf("\n");
+
+	for (i = 0; i < NUM; i++)
+		printf("Vector: b[%d] -> %f\n", i, b[i]);
+	printf("\n", b[i]);
+
+	for (i = 0; i < NUM; i++)
+		printf("Vector: c[%d] -> %f\n", i, c[i]);
+
+
 	return 0; 
 }
-
