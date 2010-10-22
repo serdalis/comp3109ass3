fb7851a3f0f82d5ea40018a27f98f875f5bede80
Pearse; Joshua David <Pearse; Joshua David jpea5904@ug.it.usyd.edu.au>
2010-10-22 17:55:18 +1100

    modified main.c to reflect changes

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdlib.h> 
 
 /* alignment macro: aligns a memory block a to multiplies of a */ 
-#define align(s,a) ((size_t)((s) + ((a) - 1)) & ~((size_t) (a) - 1)) 
+#define align(s,a) (((size_t)(s) + ((a) - 1)) & ~((size_t)(a) - 1))
 /* Alignment for SSE unit */ 
 #define SSE_ALIGN (16)
 /* Number of elements */ 
 #define NUM (100) 
 
+extern void foo(int, float *, float *);
 int main(void) 
 { 	
 	float *a = malloc(sizeof(float)*NUM + SSE_ALIGN),
@@ -17,9 +18,12 @@ int main(void)
 	b= 	(float *) align(b, SSE_ALIGN);
 	c= 	(float *) align(c, SSE_ALIGN);
 	
-	foo(NUM,a);
-
-	printf("%f\n%f\n", a, b);
+	//foo(NUM,a);
+	foo(NUM, a, b);
+	int i;
+	for (i = 0; i != SSE_ALIGN; i++) {
+		printf("%f\n%f\n", a[i], b[i]);
+	}
 	return 0; 
 }
 
